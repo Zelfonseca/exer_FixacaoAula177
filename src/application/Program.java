0d51ac85d933bc7c351e918070e68d11c6eb6663
José Luiz <zelfonseca@yahoo.com>
2024-01-23 20:12:32 -0300

    Criadas as classe Program e Account

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+/* Fazer um programa para ler os dados de uma conta bancária e depois realizar um saque nesta conta bancária, mostrando o novo saldo. 
+   Um saque não pode ocorrer ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de saque da conta. 
+   Conforme projeto da aula 177 
+*/
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//==============================================dados da conta===========================================
+		System.out.println("Informe os dados da conta.");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: R$ ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: R$ ");
+		double withdrawLimit = sc.nextDouble();
+		//==============================================dados da conta===========================================
+		//Como já informei todos os dados da conta para serem inseridos, irei instanciar o OBJETO CONTA abaixo
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: R$");
+		double amount = sc.nextDouble();
+		acc.withdraw(amount); // assim que insiro o valor a ser sacado da conta, eu chamo o método de saque da classe ACCOUNT
+		System.out.printf("Novo Saldo: R$ %.2f" , acc.getBalance()); 
+		
+		// realizei upload para o git hub
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
---
+++ src/entities/Account.java
@@ -0,0 +1,72 @@
+package entities;
+
+public class Account {
+	//ATRIBUTOS
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	
+	//CONSTRUTORES
+	public Account() {
+		super();
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	
+	//GET and SET
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	// todo esse comando do SETBalance eu anulo(apago) pois assim não ocrro o risco do saldo ser manipulado
+	//public void setBalance(Double balance) {
+		//this.balance = balance;
+	//}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	//public void setWithdrawnLimit(Double withdrawnLimit) {		o mesmo se aplica para o saque
+		//this.withdrawnLimit = withdrawnLimit;
+//	}
+	
+	
+	// MÉTODOS
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance = balance - amount;
+	}
+	
+	
+	
+
+}
